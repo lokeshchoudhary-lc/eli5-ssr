9a1a41db103a0b2ecdf67ae81cbea00040056121
Ritik Singh Panwar <33784820+ritiksp@users.noreply.github.com>
2023-03-05 19:07:27 +0530

    CA changes

---
+++ src/routes/ca/+page.svelte
@@ -0,0 +1,79 @@
+<div class="px-4 pb-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4 d-lg-none" src="/assets/images/ca.png" alt="" width="100%" >
+    <img class="d-block mx-auto mb-4 d-none d-lg-block" src="/assets/images/ca.png" alt="" width="50%">
+    <h1 class="display-5 fw-bold">Community Ambassadors</h1>
+    <div class="col-lg-8 mx-auto">
+      <p class="lead mb-4">The Community Ambassador (CA) program is a community-building initiative launched by Eli5. The program's primary objective is to promote awareness and engagement in eli5.club within universities and organizations. The program offers opportunities for participants to develop their skills while contributing to the growth of the community.</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <a href="https://tally.so/r/mJq6rR" class="btn btn-outline-primary btn-lg px-4">Become Community Ambassador</a>
+       
+      </div>
+    </div>
+  </div>
+
+  
+  <div class="container px-4">
+    <h2 class="pb-2 border-bottom">Benefits for Community Ambassador</h2>
+    <div class="row g-4 py-3 row-cols-1 row-cols-lg-3">
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center rounded bg-gradient fs-2 mb-3">
+            <i class="bi bi-trophy text-primary"></i>
+        </div>
+        <h3 class="fs-2">Winning Merch</h3>
+        <p>Members of the CA program have the opportunity to win exclusive ELI5 merch through various contests and initiatives.</p>
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center rounded bg-gradient fs-2 mb-3">
+            <i class="bi bi-arrows-fullscreen text-primary"></i>
+        </div>
+        <h3 class="fs-2">Networking Opportunities</h3>
+        <p>PBuild valuable connections that could lead to internships, job opportunities, or mentorship.</p>
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center rounded bg-gradient fs-2 mb-3">
+            <i class="bi bi-file-earmark-person text-primary"></i>
+        </div>
+        <h3 class="fs-2">Resume Builder</h3>
+        <p>Being a CA is a great addition to a resume as it demonstrates leadership & involvement in extracurricular activities.</p>
+      </div>
+    </div>
+
+    <div class="row g-4 row-cols-1 row-cols-lg-3">
+        <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center rounded bg-gradient fs-2 mb-3">
+                <i class="bi bi-bar-chart-line text-primary"></i>
+            </div>
+          <h3 class="fs-2">Personal Growth</h3>
+          <p>CA have the opportunity to challenge themselves, step out of their comfort zone, & gain confidence in their abilities.</p>
+        </div>
+        <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center rounded bg-gradient fs-2 mb-3">
+                <i class="bi bi-bullseye text-primary"></i>
+            </div>
+          <h3 class="fs-2">Exclusive Opportunities</h3>
+          <p>Campus ambassadors may receive exclusive access to events, products, or services from us.</p>
+        </div>
+        <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center rounded bg-gradient fs-2 mb-3">
+                <i class="bi bi-patch-check text-primary"></i>
+            </div>
+          <h3 class="fs-2">Recognition & Certification</h3>
+          <p>Every CA with successful completion of tenure get certified and also get a chance to win amazing gifts.</p>
+        </div>
+      </div>
+
+
+  </div>
+
+  <div class="container px-4 py-5">
+    <h2 class="pb-2 border-bottom">Responsibilities of  Community Ambassador</h2>
+    <ul class="list-group">
+        <li class="list-group-item py-3">Promoting ELI5 within their campus to generate engagement on the platform in terms of user signups/answers/new questions.</li>
+        <li class="list-group-item py-3">Building relationships with students, faculty, and staff to raise awareness of the organization's mission and goals.</li>
+        <li class="list-group-item py-3">Providing us feedback on student needs and interests.</li>
+        <li class="list-group-item py-3">Representing the organization professionally and ethically, adhering to our guidelines and policies.</li>
+        <li class="list-group-item py-3">Sharing content such as blogs, social media posts, and videos to promote ELI5.</li>
+        <li class="list-group-item py-3">Reporting on their progress and providing regular updates on their activities and accomplishments.</li>
+      </ul>
+      
+  </div>
