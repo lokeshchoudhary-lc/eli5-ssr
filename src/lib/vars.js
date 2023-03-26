28568deb3cce1673994724a6457e1f583cf87427
Lokesh_Choudhary <40513028+lokeshchoudhary-lc@users.noreply.github.com>
2023-03-26 11:41:31 +0530

    baseUrl changes

--- src/lib/vars.js
+++ src/lib/vars.js
@@ -1,6 +1,7 @@
 export const profileUrl = '/assets/images/profile/';
 // export const BaseUrl = 'http://127.0.0.1:8080/api/v1';
 // export const BaseUrl = 'http://localhost:8080/api/v1';
-export const BaseUrl = 'http://localhost:3000/api/v1';
-export const websiteUrl = 'http://localhost:3000/';
-// export const websiteUrl = 'https://eli5.club/';
+// export const BaseUrl = 'http://localhost:3000/api/v1';
+export const BaseUrl = 'https://backend.eli5.club/api/v1';
+// export const websiteUrl = 'http://localhost:3000/';
+export const websiteUrl = 'https://eli5.club/';
