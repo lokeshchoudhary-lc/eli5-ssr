05941557a864d95c18f6673b5b2fa1eb83fc136a
Lokesh Choudhary <lokeshchoudhary8949@gmail.com>
2023-04-05 19:35:01 +0530

    feat: gpt answer

--- src/routes/[tag]/[question]/+page.svelte
+++ src/routes/[tag]/[question]/+page.svelte
@@ -32,6 +32,7 @@
   let profileUrl = '/assets/images/profile/';
 
   let userAnswer = {};
+  let gptAnswer = {};
 
   let sortType = data.sortType;
   let userChoosenTag = data.userChoosenTag;
@@ -221,6 +222,19 @@
     }
   }
 
+  async function getGptAnswer(questionId) {
+    try {
+      const response = await axios.get(`/gptAnswer/${questionId}`);
+      // console.log(response);
+      gptAnswer = response.data;
+      if (response.data.message == 'no_answer') {
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async function submitAnswer(event) {
     textEditorHtml.subscribe((value) => {
       textAreaAnswer = value.getHTML();
@@ -393,6 +407,8 @@
         login = value;
       });
     }
+
+    await getGptAnswer(selectedQuestionId);
 
     // sortType = 'trending';
     // await getUserAnswer(selectedQuestionId);
@@ -639,6 +655,50 @@
                 </div>
               </div>
             </div>
+
+            <!-- eli5 gpt answer -->
+            {#if gptAnswer.answer !== undefined}
+              <div class="accordion mt-2" id="accordionExample">
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button
+                      class="accordion-button"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        style="color:#AE5A78"
+                        fill="currentColor"
+                        class="bi bi-stars me-3"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"
+                        />
+                      </svg>
+                      <span class="fs-6"> @Eli5GPT</span>
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseOne"
+                    class="accordion-collapse collapse show"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      {@html gptAnswer.answer}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            {/if}
+            <!-- end here -->
+
             <!-- user answer for the above question -->
             {#if userAnswer.answer !== undefined && login == true}
               <div class="card border-success mt-2 shadow-sm rounded">
