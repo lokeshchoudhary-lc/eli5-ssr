3cefce47849375978c5ffed67845bf3e7720c870
Lokesh Choudhary <lokeshchoudhary8949@gmail.com>
2023-04-19 14:30:49 +0530

    authorization token

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,17 @@
   import axios from 'axios';
   import { onMount } from 'svelte';
 
+  export let data;
+
   // axios.defaults.baseURL = 'http://127.0.0.1:8080/api/v1';
   // axios.defaults.baseURL = 'http://localhost:8080/api/v1';
   axios.defaults.baseURL = 'http://localhost:3000/api/v1';
   // axios.defaults.baseURL = 'https://backend.eli5.club/api/v1';
   axios.defaults.withCredentials = true;
+  axios.defaults.headers.common['Authorization'] = data?.token
+    ? `Bearer ${data.token}`
+    : '';
 
-  export let data;
   let url = '';
   let textAreaAskQuestion = '';
 
