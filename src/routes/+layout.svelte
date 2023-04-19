4e38ad12f7566e1992b375a9bb8c59813bc49270
Lokesh_Choudhary <40513028+lokeshchoudhary-lc@users.noreply.github.com>
2023-04-19 14:33:11 +0530

    url changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,9 @@
   export let data;
 
   // axios.defaults.baseURL = 'http://127.0.0.1:8080/api/v1';
-  axios.defaults.baseURL = 'http://localhost:8080/api/v1';
+  // axios.defaults.baseURL = 'http://localhost:8080/api/v1';
   //axios.defaults.baseURL = 'http://localhost:3000/api/v1';
-  // axios.defaults.baseURL = 'https://backend.eli5.club/api/v1';
+  axios.defaults.baseURL = 'https://backend.eli5.club/api/v1';
   axios.defaults.withCredentials = true;
   axios.defaults.headers.common['Authorization'] = data?.token
     ? `Bearer ${data.token}`
