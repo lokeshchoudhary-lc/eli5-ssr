e98d768008fa1f8a107aa0713346b32eaa7e3199
Ritik Singh Panwar <33784820+ritiksp@users.noreply.github.com>
2023-04-20 10:16:32 +0530

    b2b changes

---
+++ src/routes/showcase/eli5/+page.svelte
@@ -0,0 +1,169 @@
+<script>
+
+    let color = '#fff';
+    
+    </script>
+    
+    <section class="h-100 gradient-custom-2">
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-lg-9 col-xl-10">
+              <div class="card">
+                <div class="rounded-top text-white d-flex flex-row backdrop">
+                  <div class="ms-4 mt-5 d-flex flex-column" style="width: 150px;">
+                    <img src="/assets/images/eli5square.png"
+                      alt="Generic placeholder image" class="img-fluid img-thumbnail margin-top mb-2"
+                      style="width: 150px; z-index: 1">
+                  </div>
+                </div>
+                <div class="p-3 text-black">
+                  <div class="d-flex justify-content-end text-center py-0">
+    
+                    <div>
+                        <button type="button" class="btn btn-outline-danger like-btn"
+                        style="z-index: 1;">
+                        <i class="bi bi-heart-fill"></i> 100K+ Love
+                      </button>
+                    </div>
+                  </div>
+                </div>
+    
+                <div class="card-body py-0">
+                    <div class="ms-3">
+                        <h3 class="fw-bolder">Zomato</h3>
+                        <lead>@zomato</lead>
+                      </div>
+                </div>
+    
+                <div class="card-body text-black">
+                  <div class="mb-2 bg-light border border-light rounded p-4">
+                    <p class="lead fw-bold mb-1">Eli5 what does <span class="brand-name"> Zomato </span> do?</p>
+                    <div>
+                      <p class="font-italic mb-1">Zomato is a food app that helps you find the best restaurants with ratings and reviews. 
+                          You can order food online or book a table. It's like having a food genie in your pocket! 🍽️🍔🌮🍕</p>
+                    </div>
+                  </div>
+    
+                  <div class="container my-4">
+                    <div class="row g-2">
+                      <div class="col-sm-4">
+                        <div class="p-3 border rounded-4 shadow-sm">
+                          
+                            <a href={null} class="text-decoration-none text-dark">
+                              <i class="bi bi-instagram" style="color:#D02C77" /> Instagram
+                            </a>
+                        </div>
+                      </div>
+                      <div class="col-sm-4">
+                        <div class="p-3 border rounded-4 shadow-sm">
+                            <a href={null} class="text-decoration-none text-dark">
+                              <i class="bi bi-twitter" style="color:#00acee" /> Twitter
+                            </a>
+                        </div>
+                      </div>
+                      <div class="col-sm-4">
+                        <div class="p-3 border rounded-4 shadow-sm">
+                            <a href={null} class="text-decoration-none text-dark">
+                              <i class="bi bi-linkedin" style="color:#0072b1" /> Linkedin
+                            </a>
+                        </div>
+                      </div>
+                      <div class="col-sm-4">
+                        <div class="p-3 border rounded-4 shadow-sm">
+                            <a href={null} class="text-decoration-none text-dark">
+                              <i class="bi bi-facebook" style="color:#3b5998" /> Facebook
+                            </a>
+                        </div>
+                      </div>
+                      <div class="col-sm-4">
+                        <div class="p-3 border rounded-4 shadow-sm">
+                            <a href={null} class="text-decoration-none text-dark">
+                              <i class="bi bi-youtube" style="color:#FF0000" /> Youtube
+                            </a>
+                        </div>
+                      </div>
+    
+                      <div class="col-sm-4">
+                        <div class="p-3 border rounded-4 shadow-sm">
+                            <a href={null} class="text-decoration-none text-dark">
+                              <i class="bi bi-link-45deg" style="color:#FF0000" /> Other link
+                            </a>
+                        </div>
+                      </div>
+    
+                    </div>
+                  </div>
+           
+                  
+    
+                  <div class="d-flex justify-content-between align-items-center mb-2">
+                    <p class="lead fw-bold mb-0">Member also asked?</p>
+                    <p class="mb-0"><a href="#!" class="text-muted">Show all</a></p>
+                  </div>
+                  
+                  <div class="container p-0">
+                    
+                    <div class="card my-2 border border-secondary-subtle">
+                        <div class="card-header bg-white border-0 pb-1">
+                            <span class="badge rounded-pill tags">Zomato</span> <span class="badge rounded-pill tags">Eli5</span>
+                        </div>
+                        <div class="card-body pt-2">
+                          <h6 class="card-title fw-normal">What is Zomato Gold and how can I get zomato gold?</h6>
+                        </div>
+                    </div>
+                      
+                    <div class="card my-2 border border-secondary-subtle">
+                        <div class="card-header bg-white border-0 pb-1">
+                            <span class="badge rounded-pill tags">Zomato</span> <span class="badge rounded-pill tags">Eli5</span>
+                        </div>
+                        <div class="card-body py-1">
+                          <h6 class="card-title fw-normal">What is Zomato Intercity legends and does it also do international deliveries?</h6>
+                        </div>
+                    </div>
+                      
+                      
+    
+                  </div>
+                  
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+    
+    <style>
+    .margin-top{
+        margin-top:3rem;
+    }
+    
+    .backdrop{
+        background: white;  
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover; 
+        height:200px;
+    }
+    
+    .like-btn{
+        border-color: #3366FF;
+        color: #3366FF ;
+    }
+    .like-btn:hover{
+        border-color: #3366FF;
+        background-color: #3366FF;
+        color: white ;
+    }
+    
+    .brand-name{
+        color: #3366FF;
+    }
+    
+    .tags{
+        color: #3366FF;
+        border-color: #3366FF;;
+        border-style: solid;
+        border-width: 1px;
+    }
+    
+    </style>
