f75000423cfde42a9992ea0562bbdb057f535694
Lokesh_Choudhary <40513028+lokeshchoudhary-lc@users.noreply.github.com>
2023-04-25 12:24:58 +0530

    b2b fix

--- src/routes/(company)/showcase/+layout.svelte
+++ src/routes/(company)/showcase/+layout.svelte
@@ -3,8 +3,8 @@
 
   // axios.defaults.baseURL = 'http://127.0.0.1:8080/api/v1';
   // axios.defaults.baseURL = 'http://localhost:8080/api/v1';
-  // axios.defaults.baseURL = 'https://backend.eli5.club/api/v1';
-  axios.defaults.baseURL = 'http://localhost:3000/api/v1';
+   axios.defaults.baseURL = 'https://backend.eli5.club/api/v1';
+  //axios.defaults.baseURL = 'http://localhost:3000/api/v1';
   axios.defaults.withCredentials = true;
 </script>
 
